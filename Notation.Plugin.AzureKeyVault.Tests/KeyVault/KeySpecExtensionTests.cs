0eadb8093d8620091d806e1dd97edb3de98ee964
Dallas Delaney <dadelan@microsoft.com>
2026-01-08 10:59:01 -0800

    add plugin - working for signing. Not tested in VM. Use with commit in notation 725da40ceea6f7e40a1d7cf968593e355ff9051c

--- Notation.Plugin.AzureKeyVault.Tests/KeyVault/KeySpecExtensionTests.cs
+++ Notation.Plugin.AzureKeyVault.Tests/KeyVault/KeySpecExtensionTests.cs
@@ -35,5 +35,71 @@ namespace Notation.Plugin.AzureKeyVault.Client.Tests
             // Act & Assert
             Assert.Throws<ArgumentException>(() => keySpec.ToKeyVaultSignatureAlgorithm());
         }
+
+        [Theory]
+        [InlineData(KeyType.RSA, 2048, "RS256")]
+        [InlineData(KeyType.RSA, 3072, "RS384")]
+        [InlineData(KeyType.RSA, 4096, "RS512")]
+        [InlineData(KeyType.EC, 256, "ES256")]
+        [InlineData(KeyType.EC, 384, "ES384")]
+        [InlineData(KeyType.EC, 521, "ES512")]
+        public void ToSignatureAlgorithmPKCS1_ValidKeySpecs_ReturnsCorrectSignatureAlgorithm(KeyType keyType, int keySize, string expectedAlgorithm)
+        {
+            // Arrange
+            var keySpec = new KeySpec(keyType, keySize);
+            // Act
+            var signatureAlgorithm = keySpec.ToKeyVaultSignatureAlgorithmPKCS1();
+
+            // Assert
+            Assert.Equal(expectedAlgorithm, signatureAlgorithm);
+        }
+
+        [Theory]
+        [InlineData(KeyType.RSA, 1024)]
+        [InlineData(KeyType.EC, 128)]
+        public void ToSignatureAlgorithmPKCS1_InvalidKeySpecs_ThrowsArgumentException(KeyType keyType, int keySize)
+        {
+            // Arrange
+            var keySpec = new KeySpec(keyType, keySize);
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => keySpec.ToKeyVaultSignatureAlgorithmPKCS1());
+        }
+
+        [Theory]
+        [InlineData(KeyType.RSA, 2048, null, "PS256")]
+        [InlineData(KeyType.RSA, 2048, "", "PS256")]
+        [InlineData(KeyType.RSA, 2048, "rsassa-pss", "PS256")]
+        [InlineData(KeyType.RSA, 2048, "RSASSA-PSS", "PS256")]  // Case insensitive
+        [InlineData(KeyType.RSA, 2048, "rsassa-pkcs1-v1_5", "RS256")]
+        [InlineData(KeyType.RSA, 2048, "RSASSA-PKCS1-V1_5", "RS256")]  // Case insensitive
+        [InlineData(KeyType.RSA, 3072, "rsassa-pkcs1-v1_5", "RS384")]
+        [InlineData(KeyType.RSA, 4096, "rsassa-pkcs1-v1_5", "RS512")]
+        [InlineData(KeyType.EC, 256, "rsassa-pkcs1-v1_5", "ES256")]  // EC is unaffected by padding scheme
+        [InlineData(KeyType.EC, 384, "rsassa-pkcs1-v1_5", "ES384")]
+        [InlineData(KeyType.EC, 521, "rsassa-pkcs1-v1_5", "ES512")]
+        public void ToSignatureAlgorithm_WithScheme_ReturnsCorrectAlgorithm(KeyType keyType, int keySize, string? scheme, string expectedAlgorithm)
+        {
+            // Arrange
+            var keySpec = new KeySpec(keyType, keySize);
+            
+            // Act
+            var signatureAlgorithm = keySpec.ToKeyVaultSignatureAlgorithm(scheme);
+
+            // Assert
+            Assert.Equal(expectedAlgorithm, signatureAlgorithm);
+        }
+
+        [Fact]
+        public void ToSignatureAlgorithm_WithInvalidScheme_ThrowsArgumentException()
+        {
+            // Arrange
+            var keySpec = new KeySpec(KeyType.RSA, 2048);
+
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentException>(() => keySpec.ToKeyVaultSignatureAlgorithm("invalid-scheme"));
+            Assert.Contains("rsassa-pss", ex.Message);
+            Assert.Contains("rsassa-pkcs1-v1_5", ex.Message);
+        }
     }
 }
